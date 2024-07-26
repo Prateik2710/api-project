e2c7309f93d7c60547dc938fbbe150888b18877d
mri-pratikdarji <pratik.darji@mrisoftware.com>
2024-07-26 15:48:04 +0530

    Initial Commit

---
+++ HRAApplication/Controllers/EmpController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRAApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpController : ControllerBase
+    {
+        public EmpController()
+        {
+                
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
---
+++ HRAApplication/Models/HraContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HRAApplication.Models;
+
+public partial class HraContext : DbContext
+{
+    public HraContext()
+    {
+    }
+
+    public HraContext(DbContextOptions<HraContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=MRIPF19B5TJ\\MSSQL2022;Database=HRA;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.ToTable("Employee");
+
+            entity.Property(e => e.Address).HasMaxLength(50);
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+
+            //entity.HasOne(d => d.Department).WithMany(p => p.Employees)
+            //    .HasForeignKey(d => d.DepartmentId)
+            //    .HasConstraintName("FK_Employee_Department");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
